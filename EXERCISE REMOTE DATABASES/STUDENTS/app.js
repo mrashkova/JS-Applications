6034bad1b24b3cb7fcc64b82d3d3dc9942d3ab4f
Mariya Rashkova <62338721+MRashkova@users.noreply.github.com>
2020-11-16 10:09:59 +0200

    Update app.js

--- EXERCISE REMOTE DATABASES/STUDENTS/app.js
+++ EXERCISE REMOTE DATABASES/STUDENTS/app.js
@@ -1,131 +1,118 @@
-const htmlSelectors = {
-    'createBtn': () => document.querySelector('#create-form > button'),
-    'firstNameInput': () => document.getElementById('firstName'),
-    'lastNameInput': () => document.getElementById('lastName'),
-    'facultyNumberInput': () => document.getElementById('facultyNumber'),
-    'gradeInput': () => document.getElementById('grade'),
-    'id': () => document.getElementById('id'),
-    'errorContainer': () => document.getElementById('error-notification'),
-    'studentsContainer': () => document.querySelector('table > tbody'),
-}
+function solve() {
+  let url = "https://students-91195.firebaseio.com/Students/.json";
+  let form = document.querySelector("table tbody");
+  // It would be best to display the students on initial load instead of making a one-time button to do that.
+  // The Submit button that I have added, re-loads the students, so another load button is redundant.
+  displayStudents();
+  function displayStudents() {
+    fetch(url)
+      .then((response) => response.json())
+      .then((obj) => {
+        // Cleaning the form, so I wont get any duplicate records displayed when I press the Submit button.
+        form.textContent = "";
+        Object.keys(obj).forEach((key) => {
+          let trEl = document.createElement("tr");
+          let tdEl = document.createElement("td");
+          let tdElTwo = document.createElement("td");
+          let tdElThree = document.createElement("td");
+          let tdElFour = document.createElement("td");
+          let tdElFive = document.createElement("td");
+          tdEl.innerText = obj[key].ID;
+          tdElTwo.innerText = obj[key].firstName;
+          tdElThree.innerText = obj[key].lastName;
+          tdElFour.innerText = obj[key].facultyNumber;
+          tdElFive.innerText = obj[key].grade;
+          trEl.appendChild(tdEl);
+          trEl.appendChild(tdElTwo);
+          trEl.appendChild(tdElThree);
+          trEl.appendChild(tdElFour);
+          trEl.appendChild(tdElFive);
+          form.appendChild(trEl);
 
-htmlSelectors['createBtn']()
-    .addEventListener('click', createStudent);
+          // I love this sorting method
+          let sortedArr = [...form.children].sort(
+            (a, b) => a.firstChild.innerText - b.firstChild.innerText
+          );
+          while (form.firstChild) {
+            form.removeChild(form.firstChild);
+          }
+          for (const name of sortedArr) {
+            form.appendChild(name);
+          }
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-function fetchAllStudents() {
-    fetch('https://students-exercise-cf0f7.firebaseio.com/Students/.json')
-        .then(res => res.json())
-        .then(renderStudents)
-        .catch(handleError)
-}
+  let submitButton = document.getElementById("submit");
 
-function renderStudents(studentsData) {
-    const studentsContainer = htmlSelectors['studentsContainer']();
-
-    if (studentsContainer.innerHTML != '') {
-        studentsContainer.innerHTML = ''
-    };
-
-    Object
-        .keys(studentsData)
-        .sort((a, b) => a.id - b.id)
-        .forEach(studentId => {
-            const { id, firstName, lastName, facultyNumber, grade } = studentsData[studentId];
-            
-            // studentsData.sort((a, b) => a.id - b.id);
-            const tableRow = createDOMElement('tr', '', {}, {},
-                createDOMElement('td', id, {}, {}),
-                createDOMElement('td', firstName, {}, {}),
-                createDOMElement('td', lastName, {}, {}),
-                createDOMElement('td', facultyNumber, {}, {}),
-                createDOMElement('td', grade, {}, {}),
-            );
-            
-            studentsContainer.appendChild(tableRow);
-            
-        })                
-}
-
-function createStudent(e) {
+  submitButton.addEventListener("click", (e) => {
     e.preventDefault();
+    let id = document.getElementById("ID");
+    let firstName = document.getElementById("first-name");
+    let lastName = document.getElementById("last-name");
+    let facultyNumber = document.getElementById("faculty-number");
+    let grade = document.getElementById("grade");
 
-    const id = htmlSelectors['id']();
-    const firstName = htmlSelectors['firstNameInput']();
-    const lastName = htmlSelectors['lastNameInput']();
-    const facultyNumber = htmlSelectors['facultyNumberInput']();
-    const grade = htmlSelectors['gradeInput']();
+    // That way we will have correct Grade values
+    if (
+      Number(id.value.trim()) &&
+      firstName.value.trim() !== "" &&
+      lastName.value.trim() !== "" &&
+      Number(facultyNumber.value.trim()) &&
+      Number(grade.value.trim()) >= 2 &&
+      Number(grade.value.trim()) <= 6
+    ) {
+      // That way we will have unique IDs
+      if (
+        [...form.children].find((el) => el.firstChild.innerText == id.value)
+      ) {
+        alert("That ID is already taken!");
+        return;
+      }
+      // That way we will have unique Faculty Numbers
+      if (
+        [...form.children].find(
+          (el) => el.children[3].innerText == facultyNumber.value
+        )
+      ) {
+        alert("That Faculty Number is already taken!");
+        return;
+      }
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          ID: id.value,
+          firstName: firstName.value,
+          lastName: lastName.value,
+          grade: Number(grade.value).toFixed(2),
+          facultyNumber: facultyNumber.value,
+        }),
+      }).catch((error) => {
+        console.error("Error:", error);
+      });
 
-    if (id.value !== '' || firstName.value !== '' && lastName.value !== '' && facultyNumber.value !== '' && grade.value !== '') {
+      // Cleaning the input fields, to make it look all nice
+      [
+        id.value,
+        firstName.value,
+        lastName.value,
+        facultyNumber.value,
+        grade.value,
+      ] = ["", "", "", "", ""];
 
-        const initObj = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ id: id.value, firstName: firstName.value, lastName: lastName.value, facultyNumber: facultyNumber.value, grade: grade.value })
-        }
-
-        fetch('https://students-exercise-cf0f7.firebaseio.com/Students.json', initObj)
-            .then(fetchAllStudents)
-            .catch(handleError);
-
-        id.value = '';
-        firstName.value = '';
-        lastName.value = '';
-        facultyNumber.value = '';
-        grade.value = '';
+      // I set this timeout to allow some time for the POST request to be completed before making a GET request
+      setTimeout(() => {
+        displayStudents();
+      }, 200);
     } else {
-        const error = ({ message: '' });
-
-        if (firstName.value === '') {
-            error.message += 'You must fill the First Name field!';
-        }
-        if (lastName.value === '') {
-            error.message += 'You must fill the Last Name field!';
-        }
-        if (facultyNumber.value === '') {
-            error.message += 'You must fill the Faculty Number field!';
-        }
-        if (grade.value === '') {
-            error.message += 'You must fill the Grade field!';
-        }
-
-        handleError(error);
+      alert(
+        "All fields must be filled and the grade must be a value between 2 and 6!"
+      );
     }
-
-
+  });
 }
 
-function handleError(err) {
-    const errorContainer = htmlSelectors['errorContainer']();
-    errorContainer.style.display = 'block';
-    errorContainer.textContent = err.message;
-
-    setTimeout(() => {
-        errorContainer.style.display = 'none';
-    }, 5000);
-
-}
-
-function createDOMElement(type, text, attributes, events, ...children) {
-    const domElement = document.createElement(type);
-
-    if (text !== '') {
-        domElement.textContent = text;
-    }
-
-    Object.entries(attributes)
-        .forEach(([attrKey, attrValue]) => {
-            domElement.setAttribute(attrKey, attrValue);
-        });
-
-    Object.entries(events)
-        .forEach(([eventName, eventHandler]) => {
-            domElement.addEventListener(eventName, eventHandler);
-        });
-
-    domElement.append(...children);
-    // children.forEach((child) => domElement.appendChild(child));
-
-    return domElement;
-}
+solve();
