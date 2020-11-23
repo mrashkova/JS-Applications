522ea6b997361bf15a7a5ff76c233ab7bb47d100
Mariya Rashkova <62338721+MRashkova@users.noreply.github.com>
2020-11-23 09:47:52 +0200

    Delete about.js

--- EXERCISE ROUTING/Team-Manager-Skeleton/scripts/controllers/about.js
+++
@@ -1,8 +0,0 @@
-export default async function () {
-    this.partials = {
-        header: await this.load('./templates/common/header.hbs'),
-        footer: await this.load('./templates/common/footer.hbs'),
-    };
-    this.partial('./templates/about/about.hbs');
-
-}
